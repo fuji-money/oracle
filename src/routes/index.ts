07bc3533952cc98d84f5b3871271025a49dbc8f6
Louis <louis@vulpem.com>
2023-09-11 10:31:03 +0200

    cache oracle message during 30 seconds

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,57 +1,48 @@
 import express from 'express';
+import apicache from 'apicache';
 import OracleController from '../controllers/oracle';
 import PingController from '../controllers/ping';
 
-import { ECPairInterface } from 'ecpair';
-import { PriceSource } from '../domain/price-source';
+const CACHE_DURATION = '30 seconds';
 
 export default function routerFactory(
-  keyPair: ECPairInterface,
-  availableTickers: string[],
-  priceSource: PriceSource,
-  isDevelopment = false
+  oracleController: OracleController,
+  pingController: PingController
 ) {
   const router = express.Router();
 
   router.get('/ping', async (_req, res) => {
-    const controller = new PingController();
-    const response = await controller.getMessage();
+    const response = await pingController.getMessage();
     return res.send(response);
   });
 
   router.get('/oracle', (_req, res) => {
-    const controller = new OracleController(
-      keyPair,
-      availableTickers,
-      priceSource,
-      isDevelopment
-    );
-    const response = controller.getInfo();
+    const response = oracleController.getInfo();
     return res.send(response);
   });
 
-  router.get('/oracle/:ticker', async (req, res) => {
-    const controller = new OracleController(
-      keyPair,
-      availableTickers,
-      priceSource
-    );
-    try {
-      const response = await controller.getAttestationForTicker(
-        req.params.ticker,
-        req.query.timestamp as string,
-        req.query.lastPrice as string
-      );
-      if (!response)
-        return res.status(404).send({
-          message: `no attestation available for ticker ${req.params.ticker}`,
-        });
-      return res.send(response);
-    } catch (err: any) {
-      const message = err instanceof Error ? err.message : JSON.stringify(err);
-      return res.status(500).send({ message });
+  router.get(
+    '/oracle/:ticker',
+    apicache.middleware(CACHE_DURATION),
+    async (req, res) => {
+      try {
+        const response = await oracleController.getAttestationForTicker(
+          req.params.ticker,
+          req.query.timestamp as string,
+          req.query.lastPrice as string
+        );
+        if (!response)
+          return res.status(404).send({
+            message: `no attestation available for ticker ${req.params.ticker}`,
+          });
+        return res.send(response);
+      } catch (err: any) {
+        const message =
+          err instanceof Error ? err.message : JSON.stringify(err);
+        return res.status(500).send({ message });
+      }
     }
-  });
+  );
 
   return router;
 }
